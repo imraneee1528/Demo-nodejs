73932a9923b47334b4631b109dce2afe1f5bb3d6
cicd_mpower <cicd@mpower-social.com>
2022-08-31 09:13:26 +0000

    add source code chat-example

---
+++ chat-example/index.js
@@ -0,0 +1,24 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+const port = process.env.PORT || 5007;
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log("New client");
+  socket.on('message', msg => {
+    io.emit('message', msg);
+  });
+});
+
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
